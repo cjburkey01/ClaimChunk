f0497705e3469144eb48ee57cf2ce115824a774e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 16:22:06 +1000

    check owner

--- src/main/java/com/cjburkey/claimchunk/service/prereq/claim/NearChunkPrereq.java
+++ src/main/java/com/cjburkey/claimchunk/service/prereq/claim/NearChunkPrereq.java
@@ -1,5 +1,6 @@
 package com.cjburkey.claimchunk.service.prereq.claim;
 
+import com.cjburkey.claimchunk.Utils;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
@@ -12,22 +13,16 @@ public class NearChunkPrereq implements IClaimPrereq{
 
     @Override
     public boolean getPassed(@NotNull PrereqClaimData data) {
-        System.out.println("Original " + data.chunk.getX());
-        System.out.println("Original " + data.chunk.getZ());
-
         boolean nearClaimed = false;
 
         for(int x1 = -1; x1 < 2; x1++) {
             for(int z1 = -1; z1 < 2; z1++) {
-                System.out.println(x1);
-                System.out.println(z1);
                 if(nearClaimed) break;
+                if(data.claimChunk.getChunkHandler().getOwner(data.player.getWorld(), x1 + data.chunk.getX(), z1 + data.chunk.getZ()) == data.player.getUniqueId()) continue;
                 nearClaimed = data.claimChunk.getChunkHandler().isClaimed(data.chunk.getWorld().getChunkAt(x1 + data.chunk.getX(), z1 + data.chunk.getZ()));
-                System.out.println("Chunk " + (x1 + data.chunk.getX()));
-                System.out.println("Chunk " + (z1 + data.chunk.getZ()));
-                System.out.println(nearClaimed);
             }
         }
+
         return !nearClaimed;
     }
 
