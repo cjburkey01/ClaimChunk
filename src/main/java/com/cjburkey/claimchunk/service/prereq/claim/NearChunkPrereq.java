4a23ae343a10c267f8e80bd1d69d868cacde7ee8
BuildTools <unconfigured@null.spigotmc.org>
2021-09-28 15:41:19 +1000

    maybe work

--- src/main/java/com/cjburkey/claimchunk/service/prereq/claim/NearChunkPrereq.java
+++ src/main/java/com/cjburkey/claimchunk/service/prereq/claim/NearChunkPrereq.java
@@ -12,17 +12,19 @@ public class NearChunkPrereq implements IClaimPrereq{
 
     @Override
     public boolean getPassed(@NotNull PrereqClaimData data) {
-        System.out.println("Original" + data.chunk.getX());
-        System.out.println("Original" + data.chunk.getZ());
-
+        System.out.println("Original " + data.chunk.getX());
+        System.out.println("Original " + data.chunk.getZ());
 
         boolean safe = true;
 
         for(int x1 = -1; x1 < 1; x1++) {
             for(int z1 = -1; z1 < 1; z1++) {
-                safe = !data.claimChunk.getChunkHandler().isClaimed(data.chunk.getWorld().getChunkAt(x1 + data.chunk.getX(), z1 + data.chunk.getZ()));
-                System.out.println("Chunk" + (x1 + data.chunk.getX()));
-                System.out.println("Chunk" + (z1 + data.chunk.getZ()));
+                if(!safe)
+                    break;
+                safe = data.claimChunk.getChunkHandler().isClaimed(data.chunk.getWorld().getChunkAt(x1 + data.chunk.getX(), z1 + data.chunk.getZ()));
+                System.out.println("Chunk " + (x1 + data.chunk.getX()));
+                System.out.println("Chunk " + (z1 + data.chunk.getZ()));
+                System.out.println(safe);
             }
         }
         return safe;
